d91b9e9cda32c6a5fa4d8b2f31a2ae819a404e4d
MSilvin <60384924+MSilvin@users.noreply.github.com>
2021-04-19 17:10:49 +0200

    3rd commit
    - Split in 3 txt files (parameters, snakes, tracking)
    - supp unwanted characters in snakes
    - create a text files with only important information of snakes.txt
    - do the calcul of total length of each filament (per ID) + return a list

--- TSOAX_TextFile_Treatment.py
+++ TSOAX_TextFile_Treatment.py
@@ -7,21 +7,19 @@ from math import sqrt
 root = Tk()
 root.withdraw()
 
+
 #Choose your path directory
-path = askdirectory(title='Select Folder') # shows dialog box and return the path
+path = askdirectory(title='Select Working Folder') # shows dialog box and return the path
 print(path)
 os.chdir(path)
 
-#Choose the file on which you want to run this code
+#Choose the raw TSOAX File on which you want to work
 file_name = askopenfilename()
 
-'''
-#lire le fichier général
-#split le fichier en 3 parties distinctes (Parameters, Snakes, Tracking)
 
+#Read the TSOAX File and Split it in 3 distinct text files (Parameters, Snakes, Tracking)
 with open(file_name, "r") as RawFile:
     tst = RawFile.read()
-    #print(tst)
     sepfile = tst.split("Tracks")
     with open("Tracking.txt","w") as fichier3:
         fichier3.write("Tracks"+sepfile[1])
@@ -34,52 +32,59 @@ with open(file_name, "r") as RawFile:
 
     separator = ("#1")
     text = separator.join(ListPart2[1:])
-#    text="#1"+text
+
     with open("DataMicrofilament.txt","w") as fichier2:
         fichier2.write(text)
-'''
 
-#create list en séparant par $frame et dans ce cas, affiche que pour tout le premier ID
-frameAnalyse = open('DataMicrofilament.txt','r')
-txt = frameAnalyse.read().split('$frame',1)
-stringinter = txt[0].split()
-print(stringinter,'\n')
-#Definie les positions des colonnes des informations
+
+#Create txt file with only filament list (supp $frame, #1 and [...]) and save it to a new txtfile
+bad_lines = ['#', "$frame","["]
+
+with open('DataMicrofilament.txt','r') as oldfile, open('Clean List of Filament.txt', 'w') as newfile:
+    for line in oldfile:
+        if not any(bad_line in line for bad_line in bad_lines):
+            newfile.write(line)
+
+
+#Open and read this last new file to do the calcul
+frameAnalyse = open('Clean List of Filament.txt','r')
+txt = frameAnalyse.read().split()
 id = 0
 list_id=[]
-#pos = 1
-#list_pos = []
 x = 2
 list_x=[]
 y = 3
 list_y=[]
-#permet de compter le nombre d'iterations disponibles par frame
-longueurComp = int(len(stringinter)/6)
+longueurComp = int(len(txt)/6)  #count the number of iterations per frame
 
-# Recupere chaque colone et affiche son contenu
+# Stack in independant lists ID, x and y column
 for i in range (0,longueurComp):
-    list_id.append(stringinter[id])
-    #list_pos.append(stringinter[pos])
-    list_x.append(stringinter[x])
-    list_y.append(stringinter[y])
+    list_id.append(txt[id])
+    list_x.append(txt[x])
+    list_y.append(txt[y])
 
     id += 6
-    #pos += 6
     x += 6
     y += 6
-
-print(list_id)
 #print(list_id,'\n', list_x,'\n', list_y)
 
 
-'''
-#calcul des longueurs
+#Calcul part + Return a list of each ID with its total length
+listCalcFilament = []
 lenfilament = 0
-for i in range(len(list_x) - 1):
-    lenfilament += sqrt((float(list_x[i+1]) - float(list_x[i]))**2 + (float(list_y[i+1]) - float(list_y[i]))**2)
-    IDfilament = list_id[i]
-print("Le filament ID n°", IDfilament, "a pour longueur", lenfilament)
-'''
+for z in range(len(list_id)-1):
+    if list_id[z] == list_id[z+1]:
+        lenfilament += sqrt(
+                (float(list_x[z + 1]) - float(list_x[z])) ** 2 + (float(list_y[z + 1]) - float(list_y[z])) ** 2)
+        IDfilament = list_id[z]
+
+    else:
+        print("fini pour ce filament")
+        calc_distance = (IDfilament, lenfilament)
+        listCalcFilament.append(calc_distance)
+        print("Le filament ID n°", IDfilament, "a pour longueur", lenfilament)
+        lenfilament = 0
+print(listCalcFilament)
 
 
 frameAnalyse.close()
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from tkinter.filedialog import askopenfilename
 import os
 from math import sqrt
 import re
-
+import pandas as pd
 
 root = Tk()
 root.withdraw()
@@ -15,24 +15,62 @@ print(path)
 os.chdir(path)
 
 '''
-
-
-'''
 #Create txt file with only filament list
-bad_lines = ['#', "$frame"]
+bad_lines = ['#', "$frame","["]
 
 with open('DataMicrofilament.txt','r') as oldfile, open('Clean List of Filament.txt', 'w') as newfile:
     for line in oldfile:
         if not any(bad_line in line for bad_line in bad_lines):
             newfile.write(line)
 
+'''
+
 
 
 frameAnalyse = open('Clean List of Filament.txt','r')
-txt = frameAnalyse.read()#.split()
+txt = frameAnalyse.read().split()
+
+#print(txt[0])
+id = 0
+list_id=[]
+#pos = 1
+#list_pos = []
+x = 2
+list_x=[]
+y = 3
+list_y=[]
+
+#permet de compter le nombre d'iterations disponibles par frame
+longueurComp = int(len(txt)/6)
 
 
-print(txt)
+# Recupere chaque colone et affiche son contenu
+for i in range (0,longueurComp):
+    list_id.append(txt[id])
+    list_x.append(txt[x])
+    list_y.append(txt[y])
+
+    id += 6
+    #pos += 6
+    x += 6
+    y += 6
+
+#print(list_id,'\n', list_x,'\n', list_y)
+listCalcFilament = []
+lenfilament = 0
+for z in range(len(list_id)-1):
+    if list_id[z] == list_id[z+1]:
+        lenfilament += sqrt(
+                (float(list_x[z + 1]) - float(list_x[z])) ** 2 + (float(list_y[z + 1]) - float(list_y[z])) ** 2)
+        IDfilament = list_id[z]
+        print("Le filament ID n°", IDfilament, "a pour longueur", lenfilament)
+
+    else:
+        print("fini pour ce filament")
+        calc_distance = (IDfilament, lenfilament)
+        listCalcFilament.append(calc_distance)
+        lenfilament = 0
+print(listCalcFilament)
 
 
 
